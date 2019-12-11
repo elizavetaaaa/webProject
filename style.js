1d6519420148e55b106e231b6199ea9ced666012
elizavetaaaa <45116542+elizavetaaaa@users.noreply.github.com>
2019-12-11 09:22:45 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,40 @@
+
+function myFunction(){
+  document.getElementById("a2").pause();
+  document.getElementById("a3").pause();
+  document.getElementById("a4").pause();
+  document.getElementById("a5").pause();
+  
+}
+
+function myFunction1(){
+  document.getElementById("a1").pause();
+  document.getElementById("a3").pause();
+  document.getElementById("a4").pause();
+  document.getElementById("a5").pause();
+  
+}
+ 
+function myFunction2(){
+  document.getElementById("a2").pause();
+  document.getElementById("a1").pause();
+  document.getElementById("a4").pause();
+  document.getElementById("a5").pause();
+  
+}
+
+function myFunction3(){
+  document.getElementById("a2").pause();
+  document.getElementById("a3").pause();
+  document.getElementById("a1").pause();
+  document.getElementById("a5").pause();
+  
+}
+
+function myFunction4(){
+  document.getElementById("a2").pause();
+  document.getElementById("a3").pause();
+  document.getElementById("a4").pause();
+  document.getElementById("a1").pause();
+  
+}
